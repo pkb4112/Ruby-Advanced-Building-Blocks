d10d099cccf94302b0db53a39e4f57a119211f49
Peter Brandon <peterkbrandon@gmail.com>
2017-08-29 12:08:39 -0400

    bubble_sort_by added

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -1,5 +1,9 @@
 #Bubble Sort Project
 
+bubble_proc = Proc.new do |left,right|
+	left.length - right.length
+end
+
 def bubble_sort(array)
 	sort_check = false
 		while sort_check == false
@@ -20,6 +24,29 @@ def bubble_sort(array)
 		return array
 end
 
+def bubble_sort_by(array,&block)
+	
+	sort_check = false
+	while sort_check == false
+		array.each_with_index do |item,index|
+			if (array[index+1])      #makes sure you're not looking past the size of array
+				swap1 = array[index]
+				swap2 = array[index+1]
+				compare = yield(swap1,swap2)
+				if compare > 0            # swap values
+					array[index+1]=swap1
+					array[index]=swap2
+				end
+			end
+		end
+			
+		sort_check = sorted_by?(array,&block)    #test to see if the array is totally sorted, otherwise we need to keep sorting
+			
+	end
+	return array
+end
+
+
 def sorted?(array)
 	check_list = []
 	array.each_with_index do |item,index|
@@ -35,7 +62,27 @@ def sorted?(array)
     end
 end
 
+def sorted_by?(array,&block)
+	check_list = []
+	array.each_with_index do |item,index|
+		if array[index+1]
+			compare_val = yield(array[index],array[index+1])
+			check_list << compare_val
+		end
+    end
+    if check_list.any? {|item| item>1}  #if any value is greater than the value after it, return false
+    	return false
+    else
+    	return true
+    end
+end
+
+
 
 test_array = [2,7,8,3,9,11,243,56,3,8,56]
+test_array_abc = ["hey","hi","abcdefghij","helloio"]
 
-print bubble_sort(test_array)
+#print bubble_sort(test_array)
+#print bubble_sort(test_array_abc)
+
+puts bubble_sort_by(test_array_abc,&bubble_proc)
